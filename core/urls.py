bcfedfb7f4beef4bf3cbab6b0effe14f86cb7536
Toshihiro <adigungodwin2@gmail.com>
2023-04-03 16:32:10 +0100

    added the retrieve url for AdvocateViewSet

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from core import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('advocate', views.AdvocateViewSet, basename='advocate')
+# router.register(r'advocate/(?P<username>\w+)/', views.AdvocateViewSet, basename='advocate_detail')
 
 
 urlpatterns = [
-    path('', views.endpoints, name='endpoints'),
+    path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ from .models import Advocate
 from .serializers import AdvocateSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 
@@ -13,6 +15,8 @@ def endpoints(request):
     return JsonResponse(data, safe=False)
 
 class AdvocateViewSet(ViewSet):
+    queryset = Advocate.objects.all()
+    lookup_field = 'username'
 
     def create(self, request):
         """The method for creating new instances of Advocate Model"""
@@ -26,4 +30,12 @@ class AdvocateViewSet(ViewSet):
         """The method for listing all the advocates."""
         advocates = Advocate.objects.all()
         serializer =AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, *args, **kwargs):
+        """The method for retrieving a specific advocate"""
+        queryset = Advocate.objects.all()
+        username = self.kwargs['username']
+        advocate = get_object_or_404(queryset, username=username)
+        serializer = AdvocateSerializer(advocate)
         return Response(serializer.data, status=status.HTTP_200_OK)
