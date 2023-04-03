6507973ea1a1eeef4a1a6d5cac40faffe45d27fb
Toshihiro <adigungodwin2@gmail.com>
2023-04-03 15:44:09 +0100

    added the create method for AdvocateViewSet

--- core/views.py
+++ core/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from .models import Advocate
+from .serializers import AdvocateSerializer
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
 
 def endpoints(request):
     data = ['/advocates', 'advocates/:username']
     return JsonResponse(data, safe=False)
+
+class AdvocateViewSet(ViewSet):
+
+    def create(self, request):
+        """The method for creating new instances of Advocate Model"""
+        serializer = AdvocateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
