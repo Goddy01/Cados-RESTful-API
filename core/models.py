cccfa1e4cae03ff06c68e0ea797e0cc1b2fecc11
Toshihiro <adigungodwin2@gmail.com>
2023-04-04 17:54:55 +0100

    added Company Model

--- core/models.py
+++ core/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Company(models.Model):
+    name =      models.CharField(max_length=255, unique=True)
+    slogan =    models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name.title()
+
 class Advocate(models.Model):
     username =      models.CharField(max_length=255, unique=True)
     bio =           models.TextField(max_length=255, null=True, blank=True)
