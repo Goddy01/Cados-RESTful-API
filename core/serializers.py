ab58e82fd385cfedd7e5bf266e4e15a1e002c8a5
Toshihiro <adigungodwin2@gmail.com>
2023-04-04 17:55:58 +0100

    created CompanySerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,11 @@
 from .models import Advocate
 from rest_framework.serializers import ModelSerializer
 
+class CompanySerializer(ModelSerializer):
+    class Meta:
+        model = Advocate
+        fields = ['name', 'slogan']
+
 class AdvocateSerializer(ModelSerializer):
     class Meta:
         model = Advocate
