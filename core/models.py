9fcc1bec82d74f9535e93e50045abe297bfabf7d
Toshihiro <adigungodwin2@gmail.com>
2023-04-07 21:41:45 +0100

    added new fields to Advocate model

--- core/models.py
+++ core/models.py
@@ -11,7 +11,9 @@ class Company(models.Model):
 
 class Advocate(models.Model):
     company =       models.ForeignKey(Company, null=True, blank=True, on_delete=models.SET_NULL)
+    name =          models.CharField(max_length=255, unique=True, null=True, blank=True)
     username =      models.CharField(max_length=255, unique=True)
+    twitter =       models.URLField(null=True, blank=True)
     bio =           models.TextField(max_length=255, null=True, blank=True)
 
     def __str__(self):
