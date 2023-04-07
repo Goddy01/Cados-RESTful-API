44a5bc4f01fe5dc23e11200f6f538c6b70f62b5b
Toshihiro <adigungodwin2@gmail.com>
2023-04-07 13:31:11 +0100

    added get_employee method to CompanySeriaizer

--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,17 @@
 from .models import Advocate, Company
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 class CompanySerializer(ModelSerializer):
+    employee_count = SerializerMethodField(read_only=True)
     class Meta:
         model = Company
         fields = '__all__'
+
+    def get_employee_count(self, obj):
+        return obj.advocate_set.count()
 
 class AdvocateSerializer(ModelSerializer):
     company = CompanySerializer()
     class Meta:
         model = Advocate
-        fields = '__all__'
+        fields = ['username', 'bio', 'company']
